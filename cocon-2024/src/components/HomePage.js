f9231e63b791f1c0467e40f1ae30f8d05a71b3df
AmyCalleja <amymarie.calleja@gmail.com>
2024-04-09 17:14:30 +0200

    Initial commit with tab bar and initial homepage

---
+++ cocon-2024/src/components/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/HomePage.css'; 
+
+const HomePage = () => {
+  return (
+    <div className="home-container">
+      <div className="text-box">
+        <h1>CoCon 2024 - Chamonix Mont-Blanc, France</h1>
+        <p></p>
+        <p>We proudly present this year's CoCon which is being jointly organised by IAESTE France and IAESTE Switzerland from the 25th - 28th April 2024.
+           It is our pleasure to have you with us and be able to share IAESTE's values of international cooperation and community together. 
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
